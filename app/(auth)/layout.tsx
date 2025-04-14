bfb59153406cbc2d4a1fe3d5666eaf7d3960d262
Isuu1 <jakub.babelek@gmail.com>
2025-04-14 20:29:07 +0100

    Restricted access to auth pages when user is logged in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,18 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { createClient } from "@/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = await createClient();
+  const { data } = await supabase.auth.getUser();
+
+  if (data.user) {
+    redirect("/");
+  }
+
   return (
     <div className="auth-layout">
       <div style={{ flex: "0 1 50%" }}></div>
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,5 @@
 import SignupForm from "@/features/auth/components/SignupForm";
-import { createClient } from "@/supabase/server";
 
 export default async function Page() {
-  const supabase = await createClient();
-  const { data, error } = await supabase.auth.getUser();
-  console.log("data", data);
-  console.log("error", error);
   return <SignupForm />;
 }
