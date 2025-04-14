666e6d8e9c81014e43406ca45bc1666d0462cb68
Isuu1 <jakub.babelek@gmail.com>
2025-04-14 11:26:55 +0100

    Added action and onSubmit props to form component

--- shared/components/ui/Form.tsx
+++ shared/components/ui/Form.tsx
@@ -5,10 +5,16 @@ import styles from "./Form.module.scss";
 
 interface FormProps {
   children: React.ReactNode;
+  action?: (formData: FormData) => void;
+  onSubmit?: (e: React.FormEvent) => void;
 }
 
-const Form: React.FC<FormProps> = ({ children }) => {
-  return <form className={styles.form}>{children}</form>;
+const Form: React.FC<FormProps> = ({ children, action, onSubmit }) => {
+  return (
+    <form action={action} onSubmit={onSubmit} className={styles.form}>
+      {children}
+    </form>
+  );
 };
 
 export default Form;
