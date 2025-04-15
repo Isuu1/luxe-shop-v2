ebe4f7b0b477dd3b5d3de313dc490332c14de4a4
Isuu1 <jakub.babelek@gmail.com>
2025-04-15 18:49:55 +0100

    Added BackLink component in user layout

--- features/user/components/UserLayoutWrapper.tsx
+++ features/user/components/UserLayoutWrapper.tsx
@@ -3,12 +3,16 @@ import React from "react";
 //Styles
 import styles from "@/features/user/components/UserLayoutWrapper.module.scss";
 import UserNavMenu from "./UserNavMenu";
+import BackLink from "@/shared/components/BackLink";
 
 const UserLayoutWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={styles.userLayout}>
-      <UserNavMenu />
-      {children}
+      <BackLink previousPage="Account" />
+      <div className={styles.innerWrapper}>
+        <UserNavMenu />
+        {children}
+      </div>
     </div>
   );
 };
