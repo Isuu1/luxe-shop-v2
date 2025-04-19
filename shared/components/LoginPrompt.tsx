7b6b71f5d9f01c6cbffe08ed40745a56d8b6db7a
Isuu1 <jakub.babelek@gmail.com>
2025-04-19 16:00:28 +0100

    Adjusted LoginPrompt animation

--- shared/components/LoginPrompt.tsx
+++ shared/components/LoginPrompt.tsx
@@ -11,10 +11,10 @@ import { createPortal } from "react-dom";
 
 const loginPromptVariants = {
   visible: {
-    opacity: 1,
+    scale: 1,
   },
   hidden: {
-    opacity: 0,
+    scale: 0,
   },
 };
 
