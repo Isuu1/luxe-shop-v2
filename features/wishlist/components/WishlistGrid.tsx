1ae715e9c4ba1b1527870ed953e021266faab935
Isuu1 <jakub.babelek@gmail.com>
2025-04-26 16:07:43 +0100

    Added information when wishlist is empty

--- features/wishlist/components/WishlistGrid.tsx
+++ features/wishlist/components/WishlistGrid.tsx
@@ -11,7 +11,12 @@ import styles from "./WishlistGrid.module.scss";
 const WishlistGrid = () => {
   const { wishlist } = useWishlist();
 
-  return (
+  return wishlist.length === 0 ? (
+    <div className={styles.emptyWishlist}>
+      <h2>Your wishlist is empty</h2>
+      <p>Add products to your wishlist to see them here.</p>
+    </div>
+  ) : (
     <div className={styles.wishlistGrid}>
       {wishlist.map((product) => (
         <ProductCard key={product._id} product={product} />
